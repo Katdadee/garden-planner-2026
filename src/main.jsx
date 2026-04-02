1ef7e7465eb37a7f6ba16d55821ea6f7b04d516d
Antonio McCarver <admccarver@gmail.com>
2026-04-01 19:15:54 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GardenApp from './GardenApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GardenApp />
-  </React.StrictMode>,
-)
